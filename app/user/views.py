707faa83c340dd3d970de36a87eddf1ffd0cce3b
bigletka <bulegenovd2002@mail.ru>
2022-12-23 15:20:22 +0600

    Hell

--- app/user/views.py
+++ app/user/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
-from user.serializer import (
+from user.serializers import (
     UserSerializer,
     AuthTokenSerializer,
 )
@@ -24,7 +24,7 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class ManageUserView(generics.RetrieveAPIView):
+class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
